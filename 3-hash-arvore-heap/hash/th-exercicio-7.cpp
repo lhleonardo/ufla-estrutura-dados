ade25733a9d25890635f107d029e5df3442dfa47
Leonardo Braz <lhleonardo@hotmail.com>
2018-04-19 09:41:06 -0300

    Começa a implementação da questão 7

--- 3-hash-arvore-heap/hash/th-exercicio-7.cpp
+++ 3-hash-arvore-heap/hash/th-exercicio-7.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+
+using namespace std;
 /**
  * Implemente uma tabela hash com um número genérico n de posições, 
  * informado durante a construção da tabela, com resolução de colisões 
@@ -7,3 +10,38 @@
  * armazenado e n é o número de posições da tabela.
  *  
  * */
+ 
+ typedef string Chave;
+ typedef string Valor;
+ 
+ class No {
+     public:
+        No(Chave chave = "", Valor valor = "", No* proximo = NULL);
+     private:
+        Chave chave;
+        Valor valor;
+        No* proximo;
+ };
+ 
+ No::No(Chave key, Valor value, No* proximo) : chave(key), valor(value) {
+     this->proximo = proximo;
+ }
+ 
+ class TabelaHash {
+     public:
+        TabelaHash(unsigned int capacidade = 20);
+        ~TabelaHash();
+        
+        void adiciona(Chave chave, Valor valor);
+        bool existe(Chave chave);
+     private:
+        No** elementos;
+        int qtdInseridos;
+        int capacidade;
+        
+        int hashCode(string chave);
+ };
+ 
+ TabelaHash::TabelaHash(unsigned int cap) : capacidade(cap) {
+     this->elementos = new No*[this->capacidade];
+ }
