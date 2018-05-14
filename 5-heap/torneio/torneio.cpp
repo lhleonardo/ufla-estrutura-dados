b5c414b0d19936b7f8b0b6f4008da200e0c18ca6
leonardo braz <lhleonardo@hotmail.com>
2018-05-14 18:09:13 -0300

    Comentando o codigo'

--- 5-heap/torneio/torneio.cpp
+++ 5-heap/torneio/torneio.cpp
@@ -4,8 +4,12 @@
 
 using std::runtime_error;
 
-bool numeroValido(unsigned int numero) {
+bool Torneio::elementosValidos(unsigned int numero) {
 	return log10(numero) / log10(2);
+}
+
+int Torneio::qtdPares() {
+	return log2(this->qtdElementos);
 }
 
 Torneio::Torneio(Dado* valores, unsigned int qtdElementos) {
@@ -13,14 +17,27 @@ Torneio::Torneio(Dado* valores, unsigned int qtdElementos) {
 		throw runtime_error("Para realizar um torneio, a quantidade de elementos deve ser 2^n.");
 	}
 	
+	// a capacidade dos valores, tratando como uma arvore binaria
+	// vai ser sempre o dobro de elementos informados menos um. 
+	// ou seja, a arvore binaria que representa um torneio de 8 valores
+	// terá 15 nós.
 	this->capacidade = 2*qtdElementos-1;
+	
 	this->elementos = new Dado*[this->capacidade];
 	this->qtdElementos = qtdElementos;
+	
+	// inicializa os valores
 	for(unsigned int i = 0; i < this->capacidade; i++) {
 		this->elementos[i] = NULL;
 	}
 	
-	for(unsigned int i = qtdElementos-1; i >= 0; i--) {
+	// da ultima posicao, enquanto tiver valores informados devera ser armazenado
+	// na mesma ordem do que foi informado
+	
+	unsigned int limite = this->capacidade - this->qtdElementos;
+	for(unsigned int i = capacidade-1; i >= limite; i--) {
+		// crio um novo ponteiro para o valor informado, ja que o torneio vai levando o maior
+		// pra cima, entao nada mais facil que levar o endereco da memoria
 		this->elementos[i] = new int(valores[i]);
 	}
 }
--- 5-heap/torneio/torneio.hpp
+++ 5-heap/torneio/torneio.hpp
@@ -10,6 +10,8 @@ class Torneio {
 		unsigned int qtdElementos;
 		
 		void corrigeSubindo(unsigned int posicao);
+		unsigned int qtdPares();
+		bool elementosValidos();
 	public:
 		Torneio(Dado* valores, unsigned int qtdElementos);
 
